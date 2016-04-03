057de76cc95606907dbd91a9525ad56b6cd5b8d5
Owen Buckley <owen@analogstudios.net>
2016-04-03 19:38:40 -0400

    added linting and local webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
-var babelify = require('babelify');
+'use strict';
+
+var babelify = require('babelify');  // eslint-disable-line no-unused-vars
 var browserify = require('browserify');
 var gulp = require('gulp');
 var validatePipeline = require('pipeline-validate-js');
 var vinyl = require('vinyl-source-stream');
 
-gulp.task('lint:js', function() {
+gulp.task('lint', function() {
   return gulp.src('./*.js')
     .pipe(validatePipeline.validateJS());
 });
 
-gulp.task('build', function () {
+gulp.task('build', ['lint'], function () {
   return browserify({
-      entries: './app.jsx',
-      extensions: ['.jsx'],
-      debug: true
-    })
-    .transform('babelify', {
-      presets: ['es2015', 'react']
-    })
-    .bundle()
-    .pipe(vinyl('bundle.js'))
-    .pipe(gulp.dest('dest'));
+    entries: './app.jsx',
+    extensions: ['.jsx'],
+    debug: true
+  })
+  .transform('babelify', {
+    presets: ['es2015', 'react']
+  })
+  .bundle()
+  .pipe(vinyl('bundle.js'))
+  .pipe(gulp.dest('dest'));
 });
 
 gulp.task('develop', ['build'], function () {
